905c3cb2a3a1d1fb429025355626db2c4694c4e2
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-12 22:03:47 +0800

    add 主窗口

---
+++ 文件管理_文件管理模拟系统/src/Form/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            HelpForm helpForm = new HelpForm();
+            helpForm.StartPosition = FormStartPosition.CenterScreen;
+            helpForm.Show(this);
+        }
+    }
+}
