769cce6c0eda130e9e5bd75e8f1ce1585cbf8ae5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-13 16:10:11 +0800

    add 记事本窗口

---
+++ 文件管理_文件管理模拟系统/src/Form/NoteForm/NoteForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace FileManageSystem
+{
+    partial class NoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 470);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // fileRW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 468);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "fileRW";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fileRW";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.NoteForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ 文件管理_文件管理模拟系统/src/Form/NoteForm/NoteForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManageSystem
+{
+    public partial class NoteForm : Form
+    {
+        public static bool ischanged = false;       //状态位 -> 用户打开记事本是否进行编辑
+        public MainForm mainForm;
+        public string filename;
+
+        public NoteForm(string Name, MainForm parent)
+        {
+            mainForm = parent;
+            filename = Name;
+
+            InitializeComponent();
+        }
+
+        /*关闭记事本*/
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (ischanged == true)  //如果用户更改记事本 => 弹出提示框提醒保存
+            {
+                DialogResult result = MessageBox.Show("是否进行保存？", "提示信息", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    FCB nowFCB = mainForm.category.search(mainForm.category.root, filename, 0).fcb;
+                    int oldSize = nowFCB.size;
+                    int oldStart = nowFCB.start;
+
+                    string content = textBox1.Text.Trim();
+                    if (content != "")
+                        { MessageBox.Show("保存成功！"); }
+
+                    nowFCB.size = textBox1.Text.Trim().Length;
+                    nowFCB.lastModify = DateTime.Now.ToLocalTime().ToString();    //获取当前时间
+
+                    //在内存上给文件分配空间
+                    if (nowFCB.size>0)
+                    {
+                        if(mainForm.MyDisk.remain<=textBox1.Text.Trim().Length)
+                        {
+                            MessageBox.Show("磁盘空间不足！");
+                        }
+                        else
+                        {
+                            if(oldStart==-1)    //如果该文本文件之前为空(第一次修改)
+                            {
+                                mainForm.MyDisk.giveSpace(nowFCB, textBox1.Text.Trim());
+                            }
+                            else                //更新
+                            {
+                                mainForm.MyDisk.fileUpdate(oldStart, oldSize,nowFCB, textBox1.Text.Trim());
+                            }
+                        }
+                        
+                            
+                    }
+                    mainForm.fileFormInit(mainForm.currentRoot);
+                }
+                else
+                {
+                    e.Cancel = false;  //不保存直接退出
+                }
+            }
+            else
+                e.Cancel = false;   //用户未编辑直接退出
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ischanged = true;
+        }
+
+        private void NoteForm_Load(object sender, EventArgs e)
+        {
+            this.Text = filename + ".txt";
+            FCB nowFCB = mainForm.category.search(mainForm.category.root, filename, 0).fcb;
+            if (mainForm.MyDisk.getFileContent(nowFCB) != "")
+            {
+                textBox1.AppendText(mainForm.MyDisk.getFileContent(nowFCB));    //读取保存的文本文件信息
+            }
+            ischanged = false;
+        }
+    }
+}
